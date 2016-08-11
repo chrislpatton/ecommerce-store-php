418c63d28866aa4eff639fb801d818322926e447
chrislpatton <chrislpatton@gmail.com>
2016-08-11 11:00:49 -0400

    added some basic styling and header images

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -2,16 +2,21 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<title>Online Shop</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
 	<link rel="stylesheet" href="styles/styles.css">
-	<title>Online Shop</title>
 </head>
 <body>
 	<div class="main_wrapper">
-		<header>This is the header</header>
+		<header>
+			<img id="logo" src="images/storefront-logo.png" alt="">
+			<img id="banner" src="images/banner1.png" alt="">
+		</header>
 		<nav>This is the navigation menu</nav>
+		<div class="content-wrapper">
 		<aside>This is the sidebar</aside>
 		<main>This is the main content area</main>
+		</div>
 		<footer>This is the footer</footer>
 	</div>
 </body>
