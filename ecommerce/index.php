82c683638a41aacabab8cf99e4ab2b00b8bb99e0
chrislpatton <chrislpatton@gmail.com>
2016-08-11 14:05:28 -0400

    cleaned up html/css added nav/menu bar

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -7,17 +7,50 @@
 	<link rel="stylesheet" href="styles/styles.css">
 </head>
 <body>
+
+	<!-- Main wrapper starts here -->
 	<div class="main_wrapper">
+		
+		<!-- Header starts here -->
 		<header>
 			<img id="logo" src="images/storefront-logo.png" alt="">
 			<img id="banner" src="images/banner1.png" alt="">
 		</header>
-		<nav>This is the navigation menu</nav>
+		<!-- Header ends here -->
+
+		<!-- Navigation bar starts here -->
+		<nav>
+			
+			<ul id="menu">
+				<li></li> <a href="#">Home</a>
+				<li></li> <a href="#">All Products</a>
+				<li></li> <a href="#">My Account</a>
+				<li></li> <a href="#">Sign Up</a>
+				<li></li> <a href="#">Shopping Cart</a>
+				<li></li> <a href="#">Contact Us</a>
+			</ul>
+		</nav>
+		<!-- Navigation bar ends here -->
+		
+		<!-- Content wrapper starts here -->
 		<div class="content-wrapper">
-		<aside>This is the sidebar</aside>
-		<main>This is the main content area</main>
+			
+			<aside>
+				This is the sidebar
+			</aside>
+			
+			<main>
+				This is the main content area
+			</main>
+		
 		</div>
-		<footer>This is the footer</footer>
+		<!-- Content wrapper starts here -->
+
+		<footer>
+			This is the footer
+		</footer>
+	
 	</div>
+	<!-- Main wrapper starts here -->	
 </body>
 </html>
