ec47860d0f5d6199f521e63ed715988699d9cf65
chrislpatton <chrislpatton@gmail.com>
2016-08-10 22:26:08 -0400

    added styles.css

--- ecommerce/index.php
+++ ecommerce/index.php
@@ -2,9 +2,17 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+	<link rel="stylesheet" href="styles/styles.css">
 	<title>Online Shop</title>
 </head>
 <body>
-	<h1>Hello e-commerce!</h1>
+	<div class="main_wrapper">
+		<header>This is the header</header>
+		<nav>This is the navigation menu</nav>
+		<aside>This is the sidebar</aside>
+		<main>This is the main content area</main>
+		<footer>This is the footer</footer>
+	</div>
 </body>
 </html>
